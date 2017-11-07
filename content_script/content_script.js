5a7e70014fddd63e8e5e71263fcf2a45065292f5
andreea neamtgigiiiigit push git config --global user.name andreea <andreea.neamtiu@softvision.ro>
2017-11-07 11:37:14 +0200

    test cookies

---
+++ content_script/content_script.js
@@ -0,0 +1,49 @@
+//create div element in page
+var div = document.querySelector("#divContainer");
+if (!div) {
+    var newDiv = $('<div id = "divContainer" class = "divStyle" style="z-index: 9999; text-align: center; font-size: 20px; color: white; margin-top:20px; width: 300px; padding: 12px; background-color: #45925f;"></div>');
+    var header = document.querySelector("header");
+    header.append(newDiv[0]);
+    var div = document.querySelector("#divContainer");
+    console.log(div);
+}
+
+//append list item to div
+function appendLi(inputData) {
+    var listItem = createLi(inputData);
+    var div = document.querySelector("#divContainer");
+    div.append(listItem);
+}
+
+//create list item for each input element
+function createLi(inputData) {
+    return `<li class = "inputElement"> ${inputData} </li>`;
+    // var li = '<li class = "inputElement"></li>';
+    // li.innerHTML = inputData;
+    // console.log(inputData);
+    // console.log(li);
+    // return li;
+}
+
+//allowing the user to do bad things
+//append user input in div
+function createElement(request, sender, sendResponse) {
+    if(request.name == "note1") {
+        // console.log("cs", request.name);
+        // div.append(request.data);
+        appendLi(request.data);
+    }
+    else if(request.name == "note2") {
+        // console.log("cs", request.name);
+        // div.append(request.data);
+        appendLi(request.data);
+    }
+    else if(request.name == "note3") {
+        // console.log("cs", request.name);                
+        // div.append(request.data);
+        appendLi(request.data);
+    }
+}
+
+//receiving message
+browser.runtime.onMessage.addListener(createElement);
---
+++ popup/popup.js
@@ -0,0 +1,108 @@
+var inputElements = document.querySelectorAll("input");
+
+//send message to content
+function sendMyMessage(inputData, objectName) {
+    var gettingActiveTab = browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    gettingActiveTab.then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, {
+            data: inputData,
+            name: objectName
+        });
+    });
+}
+
+for (let i = 0; i < inputElements.length; i++) {
+    let inputItem = inputElements[i];
+    inputItem.addEventListener("change", function (elem) {
+
+        if (elem.target.placeholder == "note1") {
+            var inputNote1 = elem.target.value;
+            var objectName = "note1";
+            sendMyMessage(inputNote1, objectName);
+        } 
+        else if (elem.target.placeholder == "note2") {
+            var inputNote2 = elem.target.value;
+            var objectName = "note2";
+            sendMyMessage(inputNote2, objectName);
+        } 
+        else if (elem.target.placeholder == "note3") {
+            var inputNote3 = elem.target.value;
+            var objectName = "note3";
+            sendMyMessage(inputNote3, objectName);
+        }
+
+    })
+}
+
+//create cookies
+var passInput = document.getElementById("password");
+var userInput = document.getElementById("username");
+
+// API
+// COOKIES
+// CURRENT TAB
+function getActiveTab() {
+    return browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+}
+
+function logCookie(cookie) {
+    if(cookie) {
+        console.log("log the cookie");
+        console.log(cookie.name);
+        console.log(cookie.value);
+    }
+}
+
+function getCookies(tabs, cookieName) {
+    var getting = browser.cookies.get({
+        url: tabs[0].url,
+        name: cookieName
+    })
+    getting.then(logCookie);
+}
+
+//set username
+userInput.addEventListener("change", function (elem) {
+    var user = elem.target.value;
+    console.log("user input: ", user);
+    getActiveTab().then((tabs) => {
+        browser.cookies.set({
+            url: tabs[0].url,
+            name: "username",
+            value: JSON.stringify(user)
+        })
+        var userN = "username";
+        getCookies(tabs, userN);
+    });
+})
+
+//set password
+passInput.addEventListener("change", function (elem) {
+    var pass = elem.target.value;
+    console.log("pass input:", pass);
+    getActiveTab().then((tabs) => {
+        browser.cookies.set({
+            url: tabs[0].url,
+            name: "password",
+            value: JSON.stringify(pass)
+        })
+        var passName = "password";
+        getCookies(tabs, passName);
+
+        // var getPass = browser.cookies.get({
+        //     url: tabs[0].url,
+        //     name: "password"
+        // }).then((cookie) => {
+        //     console.log(cookie);
+        //     console.log(cookie.value);
+            // var passName = "password";
+            // getPass.then(logCookie(cookie));
+        });
+    
+})
